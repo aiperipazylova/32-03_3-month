ba8bb3f0256c0216802516681ffaf1f35287023e
aiperipazylova <peripro96@gmail.com>
2023-12-30 10:17:23 +0100

    Homework 7

--- project_program/js/cards.js
+++ project_program/js/cards.js
@@ -17,27 +17,27 @@ getCards(URL)
 
 const renderCards = async (url) => {
   const cards = document.querySelector('.cards')
-  const URL = 'https://jsonplaceholder.typicode.com/posts?_limit=20';
+  const URL = 'https://jsonplaceholder.typicode.com/posts?_limit=20'
   try {
-    const posts = await getCards(URL);
+    const posts = await getCards(URL)
 
     posts.forEach(post => {
-      const card = document.createElement('div');
-      card.className = 'card';
+      const card = document.createElement('div')
+      card.className = 'card'
 
-      const title = document.createElement('h2');
-      title.textContent = post.title;
+      const title = document.createElement('h2')
+      title.textContent = post.title
 
-      const description = document.createElement('p');
-      description.textContent = post.body;
+      const description = document.createElement('p')
+      description.textContent = post.body
 
-      card.appendChild(title);
-      card.appendChild(description);
-      cards.appendChild(card);
+      card.appendChild(title)
+      card.appendChild(description)
+      cards.appendChild(card)
     });
-  } catch (error) {
-    console.error(error);
+  } catch (e) {
+    console.log(e)
   }
 }
 
-window.onload = renderCards;
+window.onload = renderCards
